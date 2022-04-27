20608cb4714ddf4423db04bb3888d6e85e2fd626
Pedro Fernandes de Oliveira Neto <99500977+1pedrofneto1@users.noreply.github.com>
2022-04-27 10:51:10 -0300

    att code
    bug corrections

--- ip_validation.js
+++ ip_validation.js
@@ -15,9 +15,25 @@ const validating_ip = () => {
         }
         //If the IP is correct, return IP valid
         else if(first_block.length == 3 && second_block.length == 2 && third_block.length == 2 && fourth_block.length == 2 || first_block.length == 1 && second_block.length == 1 && third_block.length == 1 && fourth_block.length == 1){
-           let result = window.document.querySelector('div#result')
-           result.innerHTML = "IP Valid"
-           result.style.color = "rgb(27, 216, 27)"
+            first_block = Number(first_block)
+            second_block = Number(second_block)
+            third_block = Number(third_block)
+            fourth_block = Number(fourth_block)
+            //Validate IP to not contain letters
+            if(isNaN(first_block) || isNaN(second_block) || isNaN(third_block) || isNaN(fourth_block)){
+                window.document.querySelector('div#result').innerHTML = ""
+                window.alert('Invalid IP! Your IP cannot contain letters!')
+            }   
+            //Validating IP not to be greater than 255 or less than 0  
+            else if(first_block > 255){
+                window.document.querySelector('div#result').innerHTML = ""
+                window.alert('Invalid IP! Your first IP block cannot go beyond 255')
+            }
+            else if(first_block >= 0 || first_block <= 255){
+                let result = window.document.querySelector('div#result')
+                result.innerHTML = "IP Valid"
+                result.style.color = "rgb(27, 216, 27)"
+            }
         }
         else{
             window.document.querySelector('div#result').innerHTML = ""
